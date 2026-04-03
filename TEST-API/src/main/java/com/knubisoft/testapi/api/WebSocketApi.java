3573329183c68664bb2c07631d26438f4f292118
Kostenko Vadym <v.kostenko@knubisoft.com>
2026-04-03 13:46:01 +0300

    WEBSOCKET Standard -general -variable -variations
    STOMP
    -subscribe
    --ping
    --topic
    -variable
    -variations

--- TEST-API/src/main/java/com/knubisoft/testapi/api/WebSocketApi.java
+++ TEST-API/src/main/java/com/knubisoft/testapi/api/WebSocketApi.java
@@ -1,6 +1,5 @@
 package com.knubisoft.testapi.api;
 
-import com.knubisoft.testapi.dto.websocket.stomp.StompSelfMessage;
 import com.knubisoft.testapi.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,12 +9,15 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @ApiResponses(value = {
-        @ApiResponse(responseCode = "404", description = "Not Found", content = {@Content(
-                schema = @Schema(implementation = ErrorResponse.class))}),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = {@Content(
-                schema = @Schema(implementation = ErrorResponse.class))}),
-        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content(
-                schema = @Schema(implementation = ErrorResponse.class))})
+        @ApiResponse(responseCode = "404", description = "Not Found", content = {
+                @Content(schema = @Schema(implementation = ErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = {
+                @Content(schema = @Schema(implementation = ErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {
+                @Content(schema = @Schema(implementation = ErrorResponse.class))
+        })
 })
 public interface WebSocketApi {
 
@@ -27,16 +29,4 @@ public interface WebSocketApi {
 
     @MessageMapping("/server")
     void somePeriodicMessages();
-
-    @MessageMapping("/self/create")
-    void createSelf(@RequestBody final StompSelfMessage message);
-
-    @MessageMapping("/self/get")
-    void getSelf(@RequestBody final StompSelfMessage message);
-
-    @MessageMapping("/self/delete")
-    void deleteSelf(@RequestBody final StompSelfMessage message);
-
-    @MessageMapping("/self/reset")
-    void resetSelf();
 }
--- TEST-API/src/main/java/com/knubisoft/testapi/api/impl/WebSocketController.java
+++ TEST-API/src/main/java/com/knubisoft/testapi/api/impl/WebSocketController.java
@@ -1,35 +1,31 @@
 package com.knubisoft.testapi.api.impl;
 
 import com.knubisoft.testapi.api.WebSocketApi;
-import com.knubisoft.testapi.dto.websocket.stomp.StompSelfMessage;
-import com.knubisoft.testapi.dto.websocket.stomp.StompSelfResponse;
 import lombok.Data;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
-@RestController
+@Controller
 @ConditionalOnExpression("${spring.websocket.enabled:true}")
 public class WebSocketController implements WebSocketApi {
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    private final Map<String, String> selfStorage = new ConcurrentHashMap<>();
-
+    @Override
     @SneakyThrows
     public void somePing(final String arg) {
         messagingTemplate.convertAndSend("/topic/ping", new Ping(arg));
         TimeUnit.MILLISECONDS.sleep(500);
     }
 
+    @Override
     @SneakyThrows
     public void someTest(final String arg) {
         messagingTemplate.convertAndSend("/topic/test", "some test message");
@@ -37,52 +33,10 @@ public class WebSocketController implements WebSocketApi {
         messagingTemplate.convertAndSend("/topic/test", arg);
     }
 
+    @Override
     @Scheduled(fixedDelay = 1000)
     public void somePeriodicMessages() {
         messagingTemplate.convertAndSend("/topic/server", "server periodic message");
-    }
-
-    @Override
-    public void createSelf(final StompSelfMessage message) {
-        selfStorage.put(message.getId(), message.getValue());
-        messagingTemplate.convertAndSend(
-                "/topic/self/create",
-                new StompSelfResponse("CREATED", message.getId(), message.getValue())
-        );
-    }
-
-    @Override
-    public void getSelf(final StompSelfMessage message) {
-        final String value = selfStorage.get(message.getId());
-        if (value == null) {
-            messagingTemplate.convertAndSend(
-                    "/topic/self/get",
-                    new StompSelfResponse("NOT_FOUND", message.getId(), null)
-            );
-            return;
-        }
-        messagingTemplate.convertAndSend(
-                "/topic/self/get",
-                new StompSelfResponse("FOUND", message.getId(), value)
-        );
-    }
-
-    @Override
-    public void deleteSelf(final StompSelfMessage message) {
-        selfStorage.remove(message.getId());
-        messagingTemplate.convertAndSend(
-                "/topic/self/delete",
-                new StompSelfResponse("DELETED", message.getId(), null)
-        );
-    }
-
-    @Override
-    public void resetSelf() {
-        selfStorage.clear();
-        messagingTemplate.convertAndSend(
-                "/topic/self/reset",
-                new StompSelfResponse("RESET", null, null)
-        );
     }
 
     @Data
