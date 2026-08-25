c423f9c0f26b90b2144982adfdae48fb8e484d66
hotiver <vtvt@MacBook-Pro-VT.local>
2026-08-25 16:23:16 +0300

    Fixed schema updating from TestlumFramework

--- REGRESSION/update-schema.sh
+++ REGRESSION/update-schema.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+set -e
 
 WORK_DIR=$(pwd)
-
 URL='https://github.com/TestlumFramework/Testlum.git'
+
 echo "Sync started with $URL"
 git clone $URL --branch main --depth 1 --single-branch gitcopy
 echo "Repo copied into 'gitcopy' folder"
 
+mkdir -p "$WORK_DIR/schema"
+
 echo "Cleaning schema folder"
-rm -rf "$WORK_DIR"/schema/*
+rm -rf "$WORK_DIR/schema"/*
 
 echo "Copying files from gitcopy to schema"
-SCHEMA_DIR='engine/src/test/resources/subs/schema-professional'
-cp -r "$WORK_DIR"/gitcopy/"$SCHEMA_DIR"/* "$WORK_DIR"/schema/
+SCHEMA_DIR='engine/src/main/resources/schema'
+
+cp -a "$WORK_DIR/gitcopy/$SCHEMA_DIR/." "$WORK_DIR/schema/"
 
 echo "Deleting buffer dir gitcopy"
 rm -rf gitcopy
+echo "Done!"
